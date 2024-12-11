dc6134e3e414a628278bf135ec5fd26dd886d50b
Даньков Олександр <dankov.kiev@gmail.com>
2024-12-11 22:41:25 +0200

    dynamic url from query params

--- src/Embed.jsx
+++ src/Embed.jsx
@@ -4,18 +4,22 @@ const navigation = [
   { name: 'Marketplace', href: '#' },
   { name: 'Company', href: '#' },
 ]
+// const url = 'https://lab.geen.ee/face/';
+const url = "https://dankov.vercel.app/";
 
 export default function Example() {
+  const params = new URLSearchParams(window.location.search);
+  const embedUrl = params.get('embedUrl') || url;
   return (
-    <iframe
-      src="https://lab.geen.ee/face/"
-      title="Interactive spline scene + live HTML (forked)"
-      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-      style={{
-        width: '100%',
-        height: '100%',
-      }}
-    />
-  )
+			<iframe
+				src={embedUrl}
+				title="Interactive spline scene + live HTML (forked)"
+				allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+				sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+				style={{
+					width: "100%",
+					height: "100%",
+				}}
+			/>
+		);
 }
